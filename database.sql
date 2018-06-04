1093fa7ef749a1cac9e6f16565324406dbcf3d74
Aaron Kvarnlov-Leverty <bozeman42@gmail.com>
2018-06-04 13:05:29 -0500

    updated database.sql file with all tables

--- database.sql
+++ database.sql
@@ -1,5 +1,17 @@
 CREATE TABLE "states" (
-  "id" INTEGER PRIMARY KEY,
-  "name" VARCHAR(80)
+	"state_id" VARCHAR(3) PRIMARY KEY,
+	"name" VARCHAR(80)
 );
 
+CREATE TABLE "counties" (
+	"geoid" VARCHAR(5) PRIMARY KEY,
+	"state_id" VARCHAR(3) REFERENCES "states",
+	"name" VARCHAR(80)
+);
+
+CREATE TABLE "movement" (
+	"id" SERIAL PRIMARY KEY,
+	"from" VARCHAR(5) REFERENCES "counties",
+	"to" VARCHAR(5) REFERENCES "counties",
+	"quantity" INTEGER
+);
